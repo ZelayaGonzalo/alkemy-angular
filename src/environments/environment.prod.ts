d2f811b9219b747dce50c0ffd2e394b5c318cab9
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2022-09-09 16:56:26 -0300

    added variables to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  foodApiURL:'https://api.spoonacular.com/',
+  foodApiKey:'apiKey=bb89927b2b0c43aabd77936c1f786981',
+  foodApiUser:{
+    username:'mainmenu',
+    spoonacularPassword:'tacoson33staranise',
+    hash:'10c421cd523268ee489008e6bf1e66b40ebd5dd2',
+    day:'2022-09-01',
+    dayStamp:'16619947,20'
+  },
+  loginApiUrl:'http://challenge-react.alkemy.org'
 };
